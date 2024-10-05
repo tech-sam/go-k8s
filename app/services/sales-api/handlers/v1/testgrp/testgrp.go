d4efd622e53dc429d3e123ea0211ef065ee76143
Sumit Sharma <sumit@Mac.fritz.box>
2024-10-05 10:39:21 +0200

    panics progress

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,12 +2,18 @@ package testgrp
 
 import (
 	"context"
+	"math/rand"
 	"net/http"
 
 	"github.com/tech-sam/go-k8s/foundation/web"
 )
 
 func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	if n := rand.Intn(100); n%2 == 0 {
+		//return v1.NewRequestError(errors.New("TRUSTED ERROR"), http.StatusBadRequest)
+		panic("OHH NOO PANIC")
+	}
 	status := struct {
 		Status string
 	}{
